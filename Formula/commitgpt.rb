f962da86273773d1ae8c5641db8145ac417a858c
Peter <zpregister@gmail.com>
2026-01-30 08:21:33 -0700

    chore: update Homebrew formula for v0.3.4

--- Formula/commitgpt.rb
+++ Formula/commitgpt.rb
@@ -3,8 +3,8 @@
 class Commitgpt < Formula
   desc 'A CLI AI that writes git commit messages for you'
   homepage 'https://github.com/ZPVIP/commitgpt'
-  url 'https://github.com/ZPVIP/commitgpt/archive/refs/tags/v0.3.3.tar.gz'
-  sha256 '7f9865ceb67f083d0b09cd9dc07644ec3d99b2f889a10b230b17b09c0c6cfc1e'
+  url "https://github.com/ZPVIP/commitgpt/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "3821af620c568c8e533f26397ea3163e1af902f5846444e774127ebe09c5af7d"
   license 'MIT'
 
   depends_on 'ruby'
