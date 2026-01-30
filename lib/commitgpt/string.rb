a769b039fe334b96d106d6946b4b860223367dd8
Peter <zpregister@gmail.com>
2026-01-30 08:20:37 -0700

    feat: improve CLI UX with better padding and formatting

--- lib/commitgpt/string.rb
+++ lib/commitgpt/string.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+require 'tty-prompt'
+
+# Monkey patch TTY::Prompt::List to add padding at the bottom of menus
+module TTY
+  class Prompt
+    # Monkey patch to add padding
+    class List
+      alias original_render_menu render_menu
+
+      # Override render_menu to add empty lines at the bottom
+      def render_menu
+        output = original_render_menu
+        # Add 2 empty lines at the bottom so menu doesn't stick to terminal edge
+        "#{output}\n\n"
+      end
+    end
+  end
+end
+
 # Open String to add color
 class String
   def red
